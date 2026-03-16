e4b715a8caeaa81018c659d8c885cb005c21849c
Dannavan Morrison <d_mor@outlook.com>
2026-03-16 12:33:56 -0400

    Added Home page structure

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header style={{ padding: "20px 0" }}>
+      <h2>Nassau Christian Schools</h2>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div>
+      <h1>NCS Portal</h1>
+      <p>Student and Teacher Application Portal</p>
+    </div>
+  );
+}
+
+export default Home;
