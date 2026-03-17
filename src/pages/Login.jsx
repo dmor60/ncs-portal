87dee2386eb10a9b53c73ee75c9c83dcc6376a73
Dannavan Morrison <d_mor@outlook.com>
2026-03-17 09:05:03 -0400

    Describe update

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,31 +1,21 @@
 import { useEffect, useState } from "react";
-import { signInWithRedirect, getCurrentUser } from "aws-amplify/auth";
+import { signInWithRedirect, signOut, getCurrentUser } from "aws-amplify/auth";
 
 function Login() {
   const [user, setUser] = useState(null);
 
   useEffect(() => {
     getCurrentUser()
-      .then((currentUser) => setUser(currentUser))
+      .then(setUser)
       .catch(() => setUser(null));
   }, []);
 
   const handleLogin = async () => {
-    try {
-      await signInWithRedirect();
-    } catch (error) {
-      console.error("Login error:", error);
-      alert("Login failed. Check console.");
-    }
+    await signInWithRedirect();
   };
 
-  const handleLogout = () => {
-    const clientId = "i6utfvpaqq2g4om2f5tpbkj1b";
-    const logoutUri = "http://localhost:5173/";
-    const cognitoDomain = "https://us-east-1whancjxlh.auth.us-east-1.amazoncognito.com";
-
-    window.location.href =
-      `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
+  const handleLogout = async () => {
+    await signOut({ global: true });
   };
 
   return (
