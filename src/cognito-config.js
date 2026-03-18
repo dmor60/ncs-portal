3205606f138d9c1274cbc1f9d58dde3ff71b8a9f
Dannavan Morrison <d_mor@outlook.com>
2026-03-18 07:49:29 -0400

    Login fix

--- src/cognito-config.js
+++ src/cognito-config.js
@@ -1,12 +1,18 @@
+const isLocalhost = window.location.hostname === "localhost";
+
 const cognitoConfig = {
   region: "us-east-1",
   userPoolId: "us-east-1_whaNcjxLh",
   userPoolWebClientId: "i6utfvpaqq2g4om2f5tpbkj1b",
   oauth: {
     domain: "us-east-1whancjxlh.auth.us-east-1.amazoncognito.com",
-    scope: ["email", "openid", "profile"],
-    redirectSignIn: "http://localhost:5173/auth/callback",
-    redirectSignOut: "http://localhost:5173",
+    scope: ["email", "openid"],
+    redirectSignIn: isLocalhost
+      ? "http://localhost:5173/login"
+      : "https://main.d7l5a6qsx6h8h.amplifyapp.com/login",
+    redirectSignOut: isLocalhost
+      ? "http://localhost:5173/"
+      : "https://main.d7l5a6qsx6h8h.amplifyapp.com/",
     responseType: "code",
   },
 };
